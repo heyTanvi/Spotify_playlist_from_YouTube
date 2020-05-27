0c99163ded4f99485a9f58fc847ca65b86c57e47
Tannavi Gaurav <37728386+heyTanvi@users.noreply.github.com>
2020-05-27 21:52:43 +0530

    Create create_playlist.py

---
+++ create_playlist.py
@@ -0,0 +1,23 @@
+class createPlaylist:
+    def __init__(self):
+        pass
+
+    #logging into youtube
+    def youtube_login(self):
+        pass
+
+    #get liked video
+    def get_video(self):
+        pass
+
+    #create new playlist
+    def create_playlist(self):
+        pass
+
+    #search for song
+    def get_spotifyURL(self):
+        pass
+
+    #add into spotify playlist
+    def add_to_playlist(self):
+        pass
